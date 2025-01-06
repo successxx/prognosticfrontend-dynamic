5b09a675ebbd04f458f884f010bf4ef0672f89ab
successxx <kyle@epub.ai>
2025-01-05 21:57:24 -0500

    Delete src/WebinarView.d.ts

--- src/WebinarView.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "./WebinarView.jsx" {
-  // if you want to define any types, you can do it here, e.g.:
-  // export interface SomeProps { ... }
-}
-
-// By including this empty export, TS now sees "it's a module."
-export {};
