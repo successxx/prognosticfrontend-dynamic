7c0d764508f674b8cab979c6c4f0477bd32dcccc
successxx <kyle@epub.ai>
2025-01-05 21:56:04 -0500

    Update WebinarView.d.ts

--- src/WebinarView.d.ts
+++ src/WebinarView.d.ts
@@ -1,1 +1,7 @@
-declare module "./WebinarView.jsx";
+declare module "./WebinarView.jsx" {
+  // if you want to define any types, you can do it here, e.g.:
+  // export interface SomeProps { ... }
+}
+
+// By including this empty export, TS now sees "it's a module."
+export {};
