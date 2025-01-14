a5c898f689cc4727cf7b5d84949438db1f94a1e7
successxx <kyle@epub.ai>
2025-01-14 18:35:57 -0500

    Update WaitingRoom.tsx

--- src/WaitingRoom.tsx
+++ src/WaitingRoom.tsx
@@ -304,7 +304,7 @@ const preloadedQuestions = [
 
     // Kick things off
     setTimeout(() => {
-      addMessage("we're about to go live - Kyle will be joining us in a sec.", 'host', 'Selina', true);
+      addMessage("we're about to go live - Kyle will be joining us in a sec!", 'host', 'Selina', true);
       scheduleLocationMessages();
       scrollToBottom(chatEl);
     }, 4000);
