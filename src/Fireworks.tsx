c2cc3dfbf6ab1cd15aefeb43268a859931d93ec3
successxx <kyle@epub.ai>
2025-03-02 12:37:19 -0500

    Update Fireworks.tsx

--- src/Fireworks.tsx
+++ src/Fireworks.tsx
@@ -24,6 +24,7 @@ const Fireworks: React.FC = () => {
       "#FF4500",
     ];
 
+    // We rely on #typed-output for the bounding rect to center fireworks
     const textBox = document.getElementById("typed-output");
     if (!textBox) return;
     const rect = textBox.getBoundingClientRect();
