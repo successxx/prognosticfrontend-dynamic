f5d88782a9519124f61200e8d1a2d3f34dd1209c
successxx <kyle@epub.ai>
2025-03-02 17:15:45 -0500

    Update VideoOverlay.tsx

--- src/VideoOverlay.tsx
+++ src/VideoOverlay.tsx
@@ -28,7 +28,7 @@ const overlayItems: OverlayItem[] = [
   content: "", // The content will be populated from webinarInjectionData
   startTime: 0, // When it should start showing (in seconds)
   endTime: 11.05, // When it should stop showing (in seconds)
-  position: { x: 0.099, y: 0.335 }, // These are the estimated coordinates
+  position: { x: 0.099, y: 0.33 }, // These are the estimated coordinates
   style: {
     color: "#252525",
     fontSize: "1.5em", // This would be 1.5% of the video width
