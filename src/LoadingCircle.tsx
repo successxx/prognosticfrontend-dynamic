4b1a74c5813d99231dd17234fdf843b64f7ba7ab
successxx <kyle@epub.ai>
2025-03-02 13:27:31 -0500

    Update LoadingCircle.tsx

--- src/LoadingCircle.tsx
+++ src/LoadingCircle.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import styles from './LoadingCircle.module.css';
+import React, { useEffect, useState } from "react";
+import styles from "./LoadingCircle.module.css";
 
 const LoadingIndicator: React.FC = () => {
   const loadingMessages = [
@@ -22,44 +22,32 @@ const LoadingIndicator: React.FC = () => {
   ];
 
   const [messageIndex, setMessageIndex] = useState<number>(0);
-  const [fade, setFade] = useState<boolean>(true); // True for fade-in, false for fade-out
+  const [fade, setFade] = useState<boolean>(true);
 
   useEffect(() => {
     const updateMessage = () => {
-      setFade(false); // Start fade-out
-
+      setFade(false);
       setTimeout(() => {
-        // After fade-out completes, update the message and fade-in
-        setMessageIndex((prevIndex) => (prevIndex + 1) % loadingMessages.length);
-        setFade(true); // Trigger fade-in
-      }, 500); // Match the duration of the fade-out
+        setMessageIndex((prev) => (prev + 1) % loadingMessages.length);
+        setFade(true);
+      }, 500);
     };
-
-    // Change the message every 5 seconds
     const intervalId = setInterval(updateMessage, 5000);
-
     return () => clearInterval(intervalId);
   }, [loadingMessages.length]);
 
   return (
-    <div className={styles['prognostic-ai-demo-results-container']}>
-      <div className={styles['pai-dr-header']}>
+    <div className={styles["prognostic-ai-demo-results-container"]}>
+      <div className={styles["pai-dr-header"]}>
         Quantum Analysis In Process
       </div>
-
-      <div className={styles['pai-dr-content']}>
-        <div className={styles['pai-dr-spinner']}></div>
-        <div
-          className={`
-            ${styles['pai-dr-message']} 
-            ${fade ? styles['fade-in'] : styles['fade-out']}
-          `}
-        >
+      <div className={styles["pai-dr-content"]}>
+        <div className={styles["pai-dr-spinner"]}></div>
+        <div className={`${styles["pai-dr-message"]} ${fade ? styles["fade-in"] : styles["fade-out"]}`}>
           {loadingMessages[messageIndex]}
         </div>
       </div>
-
-      <div className={styles['pai-dr-footer']}>
+      <div className={styles["pai-dr-footer"]}>
         <img src="./assets/header.png" alt="Centered Logo" />
         <p>© {new Date().getFullYear()} Clients.ai</p>
       </div>
