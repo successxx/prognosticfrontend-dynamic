59c67d7c16bc95ee23a08e7c182838031a5ff457
successxx <kyle@epub.ai>
2025-03-02 13:37:22 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
-)
+const rootEl = document.getElementById("root");
+if (rootEl) {
+  const root = ReactDOM.createRoot(rootEl);
+  root.render(<App />);
+}
