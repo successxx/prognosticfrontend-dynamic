afc67ce337b670d7cadbb742bab1842366425eaa
successxx <kyle@epub.ai>
2025-03-04 16:58:20 -0500

    Update WebinarView.tsx

--- src/WebinarView.tsx
+++ src/WebinarView.tsx
@@ -286,7 +286,7 @@ const WebinarView: React.FC = () => {
           className={styles.videoPlayer}
         >
           <source
-            src="https://progwebinar.blob.core.windows.net/video/69.mp4"
+            src="https://progwebinar.blob.core.windows.net/video/6969.mp4"
             type="video/mp4"
           />
           Your browser does not support HTML5 video.
