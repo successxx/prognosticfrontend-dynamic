e0edad3c6f8421a94f9a4b061e5e271aa6e30f9a
successxx <kyle@epub.ai>
2025-03-10 12:48:03 -0400

    Update LoadingCircle.tsx

--- src/LoadingCircle.tsx
+++ src/LoadingCircle.tsx
@@ -749,7 +749,7 @@ function NewAnalysis() {
                       transition: 'transform 0.2s ease-out'
                     }}
                   ></div>
-                  <div className={styles.gaugeValue}>81% Left on table</div>
+                  <div className={styles.gaugeValue}>81% Inefficiency Detected...</div>
                 </div>
               </div>
             </>,
